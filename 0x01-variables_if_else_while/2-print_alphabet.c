17fdb4824c6fa3412d20ca128e3db74e1798a310
bisoul1984 <fikertetadesse1403@gmail.com>
2023-06-18 07:45:00 -0700

    update

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,9 +10,10 @@ int main(void)
 	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
-	for (i = 0; i <= 26; i++)
+	for (i = 0; i < 26; i++)
 	{
 		putchar(alphabet[i]);
 	}
+	putchar('\n');
 	return (0);
 }
