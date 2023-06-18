efbdcc50ad26c7184d358f65023fa8c905a62662
bisoul1984 <fikertetadesse1403@gmail.com>
2023-06-18 08:44:39 -0700

    update

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	char alphabet[24] = "abcdfghijklmnoqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 24; i++)
+	{
+		putchar(alphabet[i]);
+	}
+	putchar('\n');
+	return (0);
+}
