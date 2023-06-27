4994e7cd16d271c8616e93688a657c19e27d4e46
bisoul1984 <fikertetadesse1403@gmail.com>
2023-06-27 05:27:13 -0700

    update

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - print a string in reverse order
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int sum = 0;
+	int i;
+
+	while (*s != '\0')
+	{
+		sum++;
+		*s++;
+	}
+
+	for (i = sum; i > 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
