2071b9d6c17ed9407921b80015c321ef47e1c24f
bisoul1984 <fikertetadesse1403@gmail.com>
2023-06-27 04:54:40 -0700

    update

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - return the length of a string
+ * @s: string
+ * Return:length
+ */
+int _strlen(char *s)
+{
+	int sum;
+
+	while (*s != '\0')
+	{
+		sum++;
+		s++;
+	}
+}
+
