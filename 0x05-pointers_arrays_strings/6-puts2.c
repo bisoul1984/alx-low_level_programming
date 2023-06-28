e2748c3cd7be74183d27836743d9a1c5460396d8
bisoul1984 <fikertetadesse1403@gmail.com>
2023-06-27 18:48:53 -0700

    update

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: string
+ * Return: string in reverse order
+ */
+void rev_string(char *s)
+{
+	int sum = 0;
+	int x = 0;
+	int *t = str;
+	int y;
+
+	while (*t != '\0')
+	{
+		t++;
+		sum++;
+	}
+	x = sum - 1;
+
+	for (y = 0; y <= x; y++)
+	{
+		if (y % 2 == 0)
+		{
+			_putchar(str[0]);
+		}
+	}
+	_putchar('\n');
+}
