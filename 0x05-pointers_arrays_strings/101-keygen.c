f32361293d236c6d9de776f8d4bebb808a2834ed
bisoul1984 <fikertetadesse1403@gmail.com>
2023-06-29 04:09:17 -0700

    update

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generate random passwords
+ * for the program 101-crackme
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int password[100];
+	int x, sum, y;
+
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (x = 0; x < 100; x++)
+	{
+		password[x] = rand() % 78;
+		sum += (password[x] + '0');
+		putchar(password[x] + '0');
+
+		if ((2772 - sum) - '0' < 78)
+		{
+			y = 2772 - sum - '0';
+			sum += y;
+			putchar(n + '0');
+			break;
+		}
+	}
+}
