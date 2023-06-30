fbfa23d095e3777879afa0ded602589f49ff8270
bisoul1984 <fikertetadesse1403@gmail.com>
2023-06-30 04:20:46 -0700

    update

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - write char c to stdiou
+ * @c: character to be printed
+ *
+ * Return: on success one
+ * On error, -1 is returned
+ */
+
+int _putchar( char h)
+{
+	return (write(1, 1&h, 1);
+			}
