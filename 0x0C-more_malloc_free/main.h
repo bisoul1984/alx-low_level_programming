9ce82da70ea031b8df56f0334b142ce2529a0049
bisoul1984 <fikertetadesse1403@gmail.com>
2023-07-14 19:05:06 -0700

    update

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void errors(void);
-int strlen(char *s);
-int digit(char *s);
+int _strlen(char *s);
+int is_digit(char *s);
 
 #endif
