d863a784f519da99810005111694962d7f7991e4
bisoul1984 <fikertetadesse1403@gmail.com>
2023-07-17 05:37:46 -0700

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,9 +18,8 @@ struct dog
 };
 
 /**
- * dog_t - typdef for struct dog
+ * dog_t - typedef for struct dog
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
