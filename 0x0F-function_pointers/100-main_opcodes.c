2ec5f86a9e71ea661ca215dd8cb2c3ae156b87cc
bisoul1984 <fikertetadesse1403@gmail.com>
2023-07-19 17:01:38 -0700

    update

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int bit, x;
+	char *arr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	bit = atoi(argv[1]);
+
+	if (bit < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	arr = (char *)main;
+
+	for (x = 0; x < bit; x++)
+	{
+		if (x == bit - 1)
+		{
+			printf("%02hhx\n", arr[x]);
+			break;
+		}
+		printf("%02hhx ", arr[x]);
+	}
+}
