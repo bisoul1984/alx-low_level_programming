e37117bd96a090368a6e069b72c792368eed7126
bisoul1984 <fikertetadesse1403@gmail.com>
2023-07-27 17:48:36 -0700

    update

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,3 +11,38 @@
  */
 
 list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *n;
+	list_t *temp = *head;
+	unsigned int len = 0;
+
+	while (str[len])
+	{
+		len++;
+	}
+
+	n = malloc(sizeof(list_t));
+	if (!n)
+	{
+		return (NULL);
+	}
+
+	n->str = strdup(str);
+	n->len = len;
+	n->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = n;
+		return (n);
+	}
+
+	while (temp->next)
+	{
+		temp = temp->next;
+	}
+	temp->next = n;
+
+	return (n);
+}
+
