6cbb8b7967118a3b345b895452c6815eecbb6b91
bisoul1984 <fikertetadesse1403@gmail.com>
2023-07-27 18:02:15 -0700

    update

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,13 +8,13 @@
 
 void free_list(list_t *head)
 {
-	list_t *temp;
+	list_t *fre;
 
 	while (head)
 	{
-		temp = head->next
-			temp(head->str);
-		temp(head);
-		head = temp;
+		fre = head->next;
+		free(head->str);
+		free(head);
+		head = fre;
 	}
 }
