c8ae22b43cb6ad79e13cf7956fafea14d3f59ec8
bisoul1984 <fikertetadesse1403.gmail.com>
2023-10-02 10:34:28 -0700

    update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,58 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <elf.h>
 #include "main.h"
 
-void print_error(const char *message) {
-    fprintf(stderr, "Error: %s\n", message);
-    exit(98);
-}
+/**
+ * append_text_to_file - sppend text at the end of file.
+ * @filename: pointer to the name of file.
+ * @text_content: The string to be add to the end of the file.
+ *
+ * Return: If the function fails or filename is NULL - -1.
+ *         If the file does not exist the user lacks write permissions - -1.
+ *         Otherwise - 1.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int i, x, length = 0;
 
-void display_elf_header_info(const char *file_name) {
-    int fd = open(file_name, O_RDONLY);
-    if (fd == -1) {
-        print_error("Unable to open the ELF file");
-    }
+	if (filename == NULL)
+		return (-1);
 
-    // Read the ELF header
-    Elf64_Ehdr elf_header;
-    if (read(fd, &elf_header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr)) {
-        print_error("Unable to read ELF header");
-    }
+	if (text_content != NULL)
+	{
+		for (length = 0; text_content[length];)
+			length++;
+	}
 
-    // Check if it's an ELF file
-    if (elf_header.e_ident[EI_MAG0] != ELFMAG0 || 
-        elf_header.e_ident[EI_MAG1] != ELFMAG1 ||
-        elf_header.e_ident[EI_MAG2] != ELFMAG2 ||
-        elf_header.e_ident[EI_MAG3] != ELFMAG3) {
-        print_error("Not an ELF file");
-    }
+	i = open(filename, O_WRONLY | O_APPEND);
+	x = write(i, text_content, length);
 
-    // Print ELF header information
-    printf("Magic: ");
-    for (int i = 0; i < EI_NIDENT; i++) {
-        printf("%02x ", elf_header.e_ident[i]);
-    }
-    printf("\nClass: %s\n", elf_header.e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
-    printf("Data: %s\n", elf_header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little-endian" : "Unknown");
-    printf("Version: %d\n", elf_header.e_ident[EI_VERSION]);
-    printf("OS/ABI: %d\n", elf_header.e_ident[EI_OSABI]);
-    printf("ABI Version: %d\n", elf_header.e_ident[EI_ABIVERSION]);
+	if (i == -1 || x == -1)
+		return (-1);
 
-    printf("Type: %d\n", elf_header.e_type);
-    printf("Entry point address: 0x%lx\n", (unsigned long)elf_header.e_entry);
+	close(i);
 
-    close(fd);
-}
-
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        print_error("Usage: elf_header elf_filename");
-    }
-
-    display_elf_header_info(argv[1]);
-
-    return 0;
+	return (1);
 }
